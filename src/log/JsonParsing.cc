b60365d6a3789183cf9e96ee96a61ab8dc10ad43
Alexander <aktrinh@bu.edu>
2020-04-12 11:59:18 -0400

    added timestamping function

--- src/log/JsonParsing.cc
+++ src/log/JsonParsing.cc
@@ -4,9 +4,7 @@ using json = nlohmann::json;
 using std::string;
 namespace niok {
 namespace log {
-/*
-    Still need to implement accurate timestamping for keys
-*/
+
 JsonParsing::JsonParsing(string name) {
     name_ = name;
 }
@@ -42,6 +40,51 @@ void JsonParsing::addLogToFile(string key, string value) {
     }
 
     j[key] = { value };
+
+    // output the appended json object to the file
+    std::ofstream log;
+    log.open(name_);
+    log << j;
+    log.close();
+}
+
+// adds a new log with current time as key and a given value
+void JsonParsing::addLogToFileTimestamp(string value) {
+
+    // read in the file
+    std::ifstream test(name_);
+    std::stringstream buffer;
+    buffer << test.rdbuf();
+
+    // make a json object out of the file contents
+    // and append a new key and value
+    json j;
+    if (!buffer.str().empty()) {
+      j = json::parse(buffer.str());
+    }
+
+    // get current time for timestamp
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+
+    auto timer = std::chrono::system_clock::to_time_t(now);
+
+    std::tm bt = *std::localtime(&timer);
+
+    std::ostringstream oss;
+    oss << std::put_time(&bt, "%H:%M:%S"); // HH:MM:SS
+    oss << '.' << std::setfill('0') << std::setw(3) << ms.count();
+
+    string timestamp = ss.str().substr(0, 11);
+    timestamp += oss.str();
+
+    // associate timestamp with given value
+    j[timestamp] = { value };
 
     // output the appended json object to the file
     std::ofstream log;
